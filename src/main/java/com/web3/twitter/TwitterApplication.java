088d85978a901980d567a780673909d8b5d1b46b
hongyuan <hongyuan@jingwan.com>
2024-11-27 00:00:29 +0800

    1.增加tg 初始化

--- src/main/java/com/web3/twitter/TwitterApplication.java
+++ src/main/java/com/web3/twitter/TwitterApplication.java
@@ -1,14 +1,26 @@
 package com.web3.twitter;
 
+import com.web3.twitter.bot.MyAmazingBot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class TwitterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterApplication.class, args);
+
+        TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+        // Register our bot
+        String botToken = "7907041466:AAF3wj6mI5-XH00a2k75lfBIAEmLIdSNOeQ";
+        try {
+            botsApplication.registerBot(botToken, new MyAmazingBot(botToken));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
